47d9a3ba43fb51672d67e56a01892db6475b935f
HaDo <hado@HaDos-MacBook-Pro.local>
2019-12-07 11:52:22 +0700

    add link test

--- src/test/java/crossBrowsers/RedirectorTest.java
+++ src/test/java/crossBrowsers/RedirectorTest.java
@@ -1,4 +1,21 @@
 package crossBrowsers;
 
+import org.openqa.selenium.support.How;
+
 public class RedirectorTest {
+    public static void main(String[] args) {
+        Browser.open("chrome");
+        Browser.navigate("https://the-internet.herokuapp.com/redirector");
+        Browser.click(How.LINK_TEXT,"here");
+        Browser.click(How.LINK_TEXT,"200");
+        Browser.backToPreviousPage();
+        Browser.click(How.LINK_TEXT,"301");
+        Browser.backToPreviousPage();
+        Browser.click(How.LINK_TEXT,"404");
+        Browser.backToPreviousPage();
+        Browser.click(How.LINK_TEXT,"500");
+        Browser.backToPreviousPage();
+        Browser.click(How.LINK_TEXT,"here");
+
+    }
 }
