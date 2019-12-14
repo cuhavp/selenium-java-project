0ffd926fc7f7e38e2cf1ee6602640586704d05c2
HaDo <hado@HaDos-MacBook-Pro.local>
2019-12-14 09:44:59 +0700

    add WebTable class

---
+++ src/test/java/crossBrowsers/WebTable.java
@@ -0,0 +1,38 @@
+package crossBrowsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class WebTable {
+    private WebElement table;
+
+    public WebTable(WebElement table) {
+        this.table = table;
+    }
+
+    public int getTotalRows(){
+        return table.findElement(By.tagName("tbody")).findElements(By.tagName("tr")).size();
+    }
+
+    public int getTotalColumns(){
+        return table.findElements(By.tagName("th")).size();
+    }
+
+    public WebElement getCell(int rowIndex, int columnIndex){
+       return table.findElement(By.tagName("tbody"))
+               .findElements(By.tagName("tr")).get(rowIndex-1)
+               .findElements(By.tagName("td")).get(columnIndex-1);
+    }
+
+    public List<WebElement> getRow(int rowIndex){
+        return table.findElement(By.tagName("tbody"))
+                .findElements(By.tagName("tr")).get(rowIndex-1)
+                .findElements(By.tagName("td"));
+    }
+    public List<WebElement> getColumn(int columnIndex){
+        return table.findElement(By.tagName("tbody"))
+                .findElements(By.xpath(String.format("//tr/td[%d]",columnIndex)));
+    }
+}
